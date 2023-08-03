9558307d276853291a8cc1d693a483dc0725c6a2
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-03 10:49:59 -0300

    Update 06-funcoes.php

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -92,5 +92,16 @@
     ?>
     <p> Número 10: <?=verififcaNegativo(10)?></p>
     <p> Número -10: <?=verififcaNegativo(-10)?></p>
+
+
+    <!-- Função anônima -->
+    <h2>Função anônima (ou clousure, lambda)</h2>
+    <?php
+    $formataPreco = function(float $valor){
+        $precoFormatado = "R$ ".number_format($valor,2,",",".");
+        return $precoFormatado;
+    };
+    ?>
+    <p> 1000 formatado fica: <?=$formataPreco(1000)?> </p>
 </body>
 </html>
