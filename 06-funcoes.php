a04fb8523da732eeda16e0d60d64fa34b55c824f
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-03 09:38:00 -0300

    Funções com retorno e funções com parâmetro

--- 06-funcoes.php
+++ 06-funcoes.php
@@ -26,5 +26,53 @@
         <!-- dadosAutor seria constante -->
     </section>
 
+    <hr>
+
+
+    <h2>Função com retorno de dados</h2>
+    <?php
+    function dadosCurso(){
+        return "<b>Tec para informática</b>";
+    }
+    ?>
+    <p>Estamos no curso de <?=dadosCurso()?></p>
+    <p><?=dadosCurso()?> é ministrado na penha.</p>
+
+
+
+    <hr>
+    <h2>Função com parametros ou argumentos</h2>
+
+    <?php
+    function soma($valor1,$valor2,$valor3){ //são parametros não variaveis ()
+        $total = $valor1 + $valor2+ $valor3; //total é uma variavel local por isso usar return
+        return $total; //não pode chamar a <?=$total> direto sem o return
+    }
+    ?>
+    <!-- Chamando e exibindo -->
+    <p>Resultado 1: <?=soma(10,20,5)?></p>
+    <p>Resultado 2: <?=soma(10,90,55)?></p>
+
+    <!-- Chamando e Guardando o retorno -->
+    <?php
+    $resultado3 = soma(500,34,754);
+    ?>
+    <p>Resultado 3: <?=$resultado3?></p>
+    <?php if($resultado3> 100){ ?>
+        <p>Vinicius foi demitido</p>
+    <?php } ?>
+    
+    <!-- Usando função como parte da condição -->
+    <?php if(soma(2,3,10) >=10){?>
+        <p> Kaue Reprovado</p>
+    <?php } ?>
+
+    <?php
+    function saudacao($mensagem, $pessoa = "nome"){ //parametro opcional padrão se não tiver o segundo parametro
+        return "Olá, $mensagem $pessoa"; 
+    }
+    ?>
+    <p> <?=saudacao("Bom dia", "melissa")?></p>
+    <p> <?=saudacao("Bom dia")?></p>
 </body>
 </html>
