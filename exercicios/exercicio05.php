52012ffe080c26c2644fa0582fcd0d57f54f1fef
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-04 10:35:30 -0300

    Mudança na condicional (ternário)

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -12,24 +12,26 @@
 <body>
     <?php
     $nota1 = 9;
-    $nota2 = 1;
-    function media($valor1, $valor2){
+    $nota2 = 10;
+    function media(float $valor1, float $valor2):float {
         $media = ($valor1 + $valor2)/2;
         return $media;
     }?>
-    <p>A média de <?=$nota1?> e <?=$nota2?> é <?=media($nota1,$nota2)?></p>
+    <p>A média das notas <?=$nota1?> e <?=$nota2?> é <?=media($nota1,$nota2)?></p>
 
     <?php
      $resultado = media($nota1,$nota2);
 
-     function verificador($resultado){
+     function verificador(float $resultado ): string{
         if ($resultado > 6){
             return "<span class='blue'>Aprovado</span>";
-        ?>
-        <?php } 
+    
+     } 
         return "<span class='red'>Reprovado</span>";
         }
-        ?>
+
+        // return $valorMedia >= 7 ? "aprovado : "Reprovado";
+    ?>
         <p><?= verificador($resultado)?></p>
     
     
