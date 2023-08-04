c3ef41aba35ba0fac19c6ee455d22886ef92b686
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-04 11:39:48 -0300

    Array explode e implode()

--- 07-funcoes-nativas.php
+++ 07-funcoes-nativas.php
@@ -11,36 +11,69 @@
 
     <h2>Strings</h2>
     <h3>Trim()</h3>
-    <?php
-    // Remove espaços antes e depois de strings.
-    $nome = "         espaço exagerado    ";
-    $nomeSemEspaco = trim($nome);
-    ?>
+<?php
+// Remove espaços antes e depois de strings.
+$nome = "         espaço exagerado    ";
+$nomeSemEspaco = trim($nome);
+?>
     <pre> <?=var_dump($nome)?></pre>
     <pre> <?=var_dump($nomeSemEspaco)?></pre>
 
     <h3>str_replace()</h3>
-    <?php
-    // função que permite substituição de strings/caracteres
+<?php
+// função que permite substituição de strings/caracteres
 
-    $fraseFeia = "<p>Palavrão</p>";
-    $fraseCensurada = str_replace(
-    ["Palavrão", "Bobão"], 
-    "********", 
-    $fraseFeia
-    );
-    ?>
+$fraseFeia = "<p>Palavrão</p>";
+$fraseCensurada = str_replace(
+["Palavrão", "Bobão"], 
+"********", 
+$fraseFeia
+);
+?>
     <!-- Exibição -->
     <p><?=$fraseFeia?></p>
     <p><?=$fraseCensurada?></p>
 
-    
+    <h3>explode()</h3
+<?php
+// string para array
+$linguagens = "HTML - CSS - JS";
+$arrayLinguagens = explode("-", $linguagens);
+?>
+    <pre> <?=var_dump($linguagens)?></pre>
+    <pre> <?=var_dump($arrayLinguagens)?></pre>
+
+
+    <hr>
     <h2>Numéricas</h2>
+    <h3>min, max, round</h3>
+<?php
+$valores = [10, 5, 50, 1200, 275];
+$valorQualquer = 129.3;
+?>
 
+    <p>Maior valor: <?=max($valores)?></p>
+    <p>Menor valor: <?=min($valores)?></p>
+    <p>Arredondamento: <?=round($valorQualquer)?></p>
+
+
+    <hr>
     <h2>Arrays</h2>
+    <h3>implode()</h3>
 
+<?php
+// Função que transforma array em string
+$bandas = ["Rush", "Slayer", "Dio"];
+$textoBandas = implode( ", ", $bandas);
+?>
+    <pre><?=var_dump($bandas)?></pre>
+    <pre><?=var_dump($textoBandas)?></pre>
+
+    <hr>
     <h2>Filtros</h2>
 
+
+    <hr>
     <h2>Segurança</h2>
 </body>
 </html>
