584407e0f07cd20ae025ed758e462d17a7ba2697
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-07 10:48:02 -0300

    Exemplo de include antes do doctype

--- 01-sintaxe-geral.php
+++ 01-sintaxe-geral.php
@@ -1,10 +1,14 @@
+<?php
+include "recursos.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sintaxe geral</title>
+    <title>Sintaxe geral - <?=ESCOLA?></title>
     <style>
         p{ font-family: "Verdana";}
         .destaque {text-transform: uppercase;}
