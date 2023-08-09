aac5658d9962b876238a9db798983c36801bf96d
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-09 09:36:52 -0300

    Usando filter
    Usando filter para impedir que seja possível enviar valores não desejados

--- processa-post.php
+++ processa-post.php
@@ -21,27 +21,48 @@
         <p><a href="10-formulario.html">Voltar</a></p>
 <?php
     }else {
-        $nome = $_POST["nome"];
-        $email = $_POST["email"];
-        $mensagem = $_POST["mensagem"];
-        $idade = $_POST["idade"];
-        //se existe
-        if (isset($_POST["interesses"])) {
-            $interesses =$_POST["interesses"];
-        }else{
-            $interesses = array(); // = [];
-        }
-
-        // operrador de coalescência: ??
+        $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+        // $nome = filter_var($_POST["nome"], FILTER_SANITIZE_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        $idade = filter_input(INPUT_POST, "idade", FILTER_SANITIZE_NUMBER_INT);
+        $mensagem = filter_input(INPUT_POST, "mensagem", FILTER_SANITIZE_SPECIAL_CHARS);
         // $interesses = $_POST["interesses"] ?? [];
+        //Neste caso é um ARRAY
+        $interesses = filter_var_array($_POST["interesses"] ?? [], FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+        //se existe
+        /* if (isset($_POST["interesses"])) {
+               $interesses =$_POST["interesses"];
+           }else{
+               $interesses = array(); ou = [];
+           }*/
+
+        /* operador de coalescência: ??
+         $interesses = $_POST["interesses"] ?? [];*/
 ?>
 
     <h2>Dados: </h2>
     <ul>
         <li>Nome: <?=$nome?></li>
         <li>Email: <?=$email?> </li>
-        <li>Interesses: <?=implode(", " , $interesses)?> </li><!-- É uma array então não pode ser chamado como uma variavel normal -->
         <li>Idade: <?=$idade?> </li>
+
+        <!-- apenas para que o espaço não fique vazio, esse código nem mostra "Interesses:     "-->
+        <?php if(!empty($interesses)){ ?>
+            <!-- Exibição Normal -->
+            <!-- É uma array então não pode ser chamado como uma variavel normal por isso implode()-->
+            <li>Interesses: <?=implode(", " , $interesses)?> </li>
+
+            <!-- Exibição formato de Lista -->
+            <li>Interesses: 
+                <ul>
+                    <?php foreach($interesses as $interesse){?>
+                        <li><?=$interesse?></li>
+                    <?php } ?>
+                </ul>
+            </li>
+        <?php } ?>
 
         <!-- Não é obrigatório, se a $mensagem não estiver VAZIA mostre li preechido  -->
         <?php if(!empty($mensagem)){ ?>
