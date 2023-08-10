eec3798734bb95c727c3a87971fb15e2304c1315
Melissa Julia L. L <108154398+melissaJll@users.noreply.github.com>
2023-08-09 21:44:59 -0300

    Update processa-06.php

--- exercicios/processa-06.php
+++ exercicios/processa-06.php
@@ -14,12 +14,24 @@ $fabricantes = ["Dell","EPCOM","Lenovo","HP"];
     if( empty($_POST["nome"]) || empty($_POST["preco"])){
 ?>
        <p>Você deve preencher <b>Nome</b> e <b>Preço!</b></p>
+       <a href="exercicio06.php">Tentar Novamente →</a>
+       <!-- ALTERAR -->
 <?php    
     } else{
-        $nome = $_POST["nome"];
-        $preco = $_POST["preco"];
-    }  
+        // Atribuição à variável de acordo com o name=""
+        $nome = filter_input(INPUT_POST, "nome", FILTER_SANITIZE_SPECIAL_CHARS);
+        $preco = filter_input(INPUT_POST, "preco", FILTER_SANITIZE_NUMBER_INT);
+        $fabricante = $_POST["fabricantes"];
+        $disponibilidade = filter_input(INPUT_POST, "disponibilidade", FILTER_SANITIZE_SPECIAL_CHARS);
+        $descricao = filter_input(INPUT_POST, "descricao", FILTER_SANITIZE_SPECIAL_CHARS);
+  
 ?>
-    
+        <!-- O que será exibido -->
+        <p><b>Nome do Produto:</b> <?=$nome?></p>
+        <p><b>Fabricante:</b> <?=$fabricante?></p>
+        <p><b>Preço:</b> R$<?=number_format($preco, 2, ",", ".")?></p>
+        <p><b>Disponibilidade:</b> <?=$disponibilidade?></p>
+        <p><b>Descrição do produto:</b> <?=$descricao?></p>
+<?php } ?>
 </body>
 </html>
