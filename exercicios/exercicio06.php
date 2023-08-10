f2cbc93d6a925de66df81bf7ec69921c2f5ca4a2
Melissa Julia L. L <melissa.lequipe@gmail.com>
2023-08-10 10:47:26 -0300

    Bootstrap finalizado

--- exercicios/exercicio06.php
+++ exercicios/exercicio06.php
@@ -6,105 +6,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio - 06</title>
     <style>
-        /* body {background: rgb(176,232,114); */
-        /* background: linear-gradient(304deg, rgba(176,232,114,1) 0%, rgba(114,209,232,1) 50%); height: 99vh} */
+         body {background: rgb(160,219,236);
+    background: linear-gradient(110deg, rgba(160,219,236,1) 30%, rgba(181,238,255,1) 99%); height: 100vh} 
         form{
         margin: 20px auto 0 auto;
         border: 1px rgb(155, 155, 155);
         border-radius: 6px;
-        width: 35%;
+        max-width: 38%;
         height: 80%;
         padding: 20px;
         background-color: rgba(253, 253, 253, 0.829);
         }
-        form input{
-            padding: 12px;
-            border-radius: 4px;
-            border: solid 1px rgb(139, 139, 139);
-        }
-        .width-80{width: 75%;}
-        html{
-        font-family: 'Roboto', sans-serif; font-size: 13px;
-        }
+        .center{margin: auto;}
+        .width-80{width: 70%;}
+        .mrg-top18{ margin-top: 18px;}
+
     </style>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 </head>
+
 <body>
 
 
     <div class="container">
         <form action="../exercicios/processa-06.php" method="post">
-            <p>
-                <label for="nome">Nome do produto:</label>
-                <input class="width-80" type="text" name="nome" id="nome" required>
-            </p>
-
-            <p>
-                <label for="fabricantes">Escolha o fabricante: </label>
-                <select id="fabricante" name="fabricante">
+            <div class="mb-3">
+                <!-- Nome Produto -->
+                <p>
+                    <label label class="form-label" for="nome">Nome do produto:</label>
+                    <input class="form-control width-80" type="text" name="nome" id="nome" required>
+                </p>
+            </div>
+            <!-- Select Fabricante -->
+            <div class="mb-3">
+                <label class="form-label" for="fabricantes">Escolha o fabricante: </label>
+                <select class="form-select" id="fabricante" name="fabricante">
                     <?php
                     foreach ($fabricantes as $fabricante){ ?>
                         <option value="<?=$fabricante;?>"><?=$fabricante?></option>
                     <?php }?>
                 </select>
-            </p>
-
-            <p>
-                <label for="preco">Preço</label><br>
-                <input type="number" name="preco" step=".01" id="preco" min="100" max="10000" required>
-            </p>
-            <div>
-                <p>Disponibilidade: </p>
+            </div>
+            
+            <!-- Preço -->
+            <label class="form-label"  for="preco" >Preço</label> 
+            <div class="input-group mb-3">
+                <span class="input-group-text">$</span>
+                    <input type="number" name="preco" step=".01" id="preco" min="100" max="10000" required>
+            </div>
+            <!-- Dsiponibilidade -->
+            <div >
+                <label label class="form-label" for="nome">Disponibilidade: </label><br>
                 <input type="radio" id="sim" name="disponibilidade" value="Sim">
                 <label for="sim">Sim</label>
                 <input type="radio" id="nao" name="disponibilidade" value="Nâo">
                 <label for="nao">Não</label>
             </div>
-            <p>
-                <label for="descricao">Descrição:</label><br>
-                <textarea name="descricao" id="descricao" cols="35"  rows="6"></textarea>
-            </p>
-            <button type="submit" name="enviar" id="enviar"> Enviar </button>
+
+            <!-- text area -->
+            <div class="mrg-top18">
+                <label for="exampleFormControlTextarea1" class="form-label">Descrição: </label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+
+            <!--  Botão -->
+            <button type="submit" class="btn bg-primary-subtle mrg-top18">Enviar</button>
         </form>
     </div>
-
-<!-- <form>
-  <div class="mb-3">
-    <label for="exampleInputNome" class="form-label">Nome do produto</label>
-    <input type="text" class="form-control" id="exampleInputNome" >
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-      <label for="disabledSelect" class="form-label">Disabled select menu</label>
-      <select id="disabledSelect" class="form-select">
-        <option>Disabled select</option>
-      </select>
-   </div>
-
-   <div class="input-group mb-3">
-        <span class="input-group-text">$</span>
-        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-        <span class="input-group-text">.00</span>
-    </div>
-
-
-   <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-    <label class="form-check-label" for="inlineRadio1"> 1</label>
-   </div>
-   <div class="form-check form-check-inline">
-    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-    <label class="form-check-label" for="inlineRadio2"> 2</label>
-  </div> <br> <br>
-
-
-   <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-  <button type="submit" class="btn btn-primary">Enviar</button>
-</form> -->
     
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
--- exercicios/processa-06.php
+++ exercicios/processa-06.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html id="processo" lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Processamento</title>
+    <style>
+        html .processo{background-color: #e3f8ff;}
+        .padding20{ padding: 20px;}
+        .mrg-top18{margin: 8px;}
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 </head>
 <body>
 <?php
@@ -15,8 +22,10 @@ $fabricantes = ["Dell","EPCOM","Lenovo","HP"];
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if( empty($_POST["nome"]) || empty($_POST["preco"])){
 ?>
-       <p>Você deve preencher <b>Nome</b> e <b>Preço!</b></p>
-       <a href="exercicio06.php">Tentar Novamente →</a>
+        <div class="alert alert-info" role="alert">
+           <p>Você deve preencher <b>Nome</b> e <b>Preço</b>!</p>
+           <a class="btn btn-light" href="exercicio06.php"> Tentar Novamente →</a>
+        </div>
 
 <?php    
     } 
@@ -30,11 +39,15 @@ else{
   
 ?>
         <!-- O que será exibido -->
-        <p><b>Nome do Produto:</b> <?=$nome?></p>
-        <p><b>Fabricante:</b> <?=$fabricante?></p>
-        <p><b>Preço:</b> R$<?=number_format($preco, 2, ",", ".")?></p>
-        <p><b>Disponibilidade:</b> <?=$disponibilidade?></p>
-        <p><b>Descrição do produto:</b> <?=$descricao?></p>
+        <div class="card text-bg-light mb-3 padding20 .mrg-top18" style="max-width: 19rem;">
+            <p><b>Nome do Produto:</b> <?=$nome?></p>
+            <p><b>Fabricante:</b> <?=$fabricante?></p>
+            <p><b>Preço:</b> R$<?=number_format($preco, 2, ",", ".")?></p>
+            <p><b>Disponibilidade:</b> <?=$disponibilidade?></p>
+            <p><b>Descrição do produto:</b> <?=$descricao?></p>
+        </div>
 <?php } } ?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
