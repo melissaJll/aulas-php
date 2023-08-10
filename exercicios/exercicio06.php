b235c3fffe1787752792d1dacb057049c8c71f27
Melissa Julia L. L <108154398+melissaJll@users.noreply.github.com>
2023-08-09 22:09:50 -0300

    bootstrap

--- exercicios/exercicio06.php
+++ exercicios/exercicio06.php
@@ -6,26 +6,28 @@
     <title>Exercicio - 06</title>
     <style>
         body {background: rgb(176,232,114);
-        background: linear-gradient(304deg, rgba(176,232,114,1) 0%, rgba(114,209,232,1) 50%); height: 98vh}
+        background: linear-gradient(304deg, rgba(176,232,114,1) 0%, rgba(114,209,232,1) 50%); height: 99vh}
         form{
-        margin: auto;
+        margin: 20px auto 0 auto;
         border: 1px rgb(155, 155, 155);
         border-radius: 6px;
         width: 35%;
         height: 80%;
         padding: 20px;
         background-color: rgba(253, 253, 253, 0.829);
-    }
-    form input{
-        padding: 12px;
-        border-radius: 4px;
-        border: solid 1px rgb(139, 139, 139);
-    }
-    .width-80{width: 75%;}
-    html{
-    font-family: 'Roboto', sans-serif; font-size: 13px;
-}
+        }
+        form input{
+            padding: 12px;
+            border-radius: 4px;
+            border: solid 1px rgb(139, 139, 139);
+        }
+        .width-80{width: 75%;}
+        html{
+        font-family: 'Roboto', sans-serif; font-size: 13px;
+        }
     </style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 </head>
 <body>
 <?php  $fabricantes = ["","Dell","EPCOM","Lenovo","HP"]; 
@@ -63,6 +65,44 @@
             <button type="submit" name="enviar" id="enviar"> Enviar </button>
         </form>
     </div>
+
+<!-- <form>
+  <div class="mb-3">
+    <label for="exampleInputNome" class="form-label">Nome do produto</label>
+    <input type="text" class="form-control" id="exampleInputNome" >
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+      <label for="disabledSelect" class="form-label">Disabled select menu</label>
+      <select id="disabledSelect" class="form-select">
+        <option>Disabled select</option>
+      </select>
+   </div>
+
+   <div class="input-group mb-3">
+        <span class="input-group-text">$</span>
+        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+        <span class="input-group-text">.00</span>
+    </div>
+
+
+   <div class="form-check form-check-inline">
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+    <label class="form-check-label" for="inlineRadio1"> 1</label>
+   </div>
+   <div class="form-check form-check-inline">
+    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+    <label class="form-check-label" for="inlineRadio2"> 2</label>
+  </div> <br> <br>
+
+
+   <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form> -->
     
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
